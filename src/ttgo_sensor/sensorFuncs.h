2957f0ee8121ec9c990ce8792512a9afd1ebabb2
expoman <maurice.guettler@web.de>
2023-06-30 16:59:02 +0200

    more refactoring
    add separate sensor_funcs.h/.cpp file

---
+++ src/ttgo_sensor/sensorFuncs.h
@@ -0,0 +1,28 @@
+#ifndef SENSORFUNCS_H
+#define SENSORFUNCS_H
+#include <Arduino.h>
+#include <SPI.h>
+//#include <Wire.h>
+#include <Adafruit_VEML7700.h>
+#include "Adafruit_BME680.h"
+#include <Adafruit_LSM6DS3TRC.h>
+
+extern Adafruit_VEML7700 veml;
+extern bool veml7700fnd;
+
+void configVeml7700();
+void printVeml7700Infos();
+
+extern Adafruit_BME680 bme;
+extern bool bme680fnd;
+
+void configBme680();
+void printBme680Infos();
+
+extern Adafruit_LSM6DS3TRC lsm6ds3trc;
+extern bool lsm6dsfnd;
+
+void configLsm6ds();
+void printLsm6dsInfos();
+
+#endif
